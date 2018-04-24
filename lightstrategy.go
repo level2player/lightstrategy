8d8251b3bac618a99f71ad65aac69933f2547f43
level2player <576102544@qq.com>
2018-04-24 15:44:03 +0800

    20180424

--- lightstrategy.go
+++ lightstrategy.go
@@ -3,11 +3,27 @@ package main
 import (
 	"lightstrategy/controllers"
 	"lightstrategy/lightmvc"
+	"github.com/spf13/cobra"
 	"log"
 	"net/http"
+	"os"
 )
-
-func main() {
+var RootCmd = &cobra.Command{
+    Use: "-v",
+    Run: func(cmd *cobra.Command, args []string) {
+        println("lightstrategy version is 0.0.1")
+	},
+}
+var UpCmd= &cobra.Command{
+	Use: "up",
+    Run: func(cmd *cobra.Command, args []string) {
+        InitHttpServer()
+	},
+}
+func init(){
+    RootCmd.AddCommand(UpCmd)
+}
+func InitHttpServer(){
 	lightmvc.RegisterController("webapinsertstockinfo", controllers.WebapInsertStockInfoController{})
 	lightmvc.RegisterController("webapifindstockinfo", controllers.WebapiFindStockInfoController{})
 	lightmvc.RegisterController("webapipropertytomvvmlight", controllers.WebapiPropertyToMvvmlightController{})
@@ -17,4 +33,10 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+func main() {
+	if err := RootCmd.Execute(); err != nil {
+        log.Println(err)
+        os.Exit(1)
+    }
 }
